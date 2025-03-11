02750e1cf26a319b199253895073d34d4e58e84b
AndreDB19 <andrediasbalbino19@gmail.com>
2025-03-11 10:30:53 -0300

    Alvos spawnando aleatoriamente

--- Shoot_Gallery/Assets/Scripts/Spawn.cs
+++ Shoot_Gallery/Assets/Scripts/Spawn.cs
@@ -1,60 +1,127 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using System.Collections;
 
 public class Spawn : MonoBehaviour
 {
 
     public GameObject AlvoNormal;
     public GameObject AlvoRapido;
-    private int count = 0;
+    public GameObject AlvoPulante;
+
+    public GameObject spawnEsquerda;
+    public GameObject spawnDireita;
+    public GameObject spawnCentro;
+
+    private int spawner;
+    private int alvo;
 
     public Alvo[] script;
-    // Update is called once per frame
-    void FixedUpdate()
+
+    void Start()
     {
-        count++;
+        StartCoroutine(Spawning());
 
-        switch (count)
-        {
-            case 300:
-                Instantiate(AlvoNormal, transform);
-                script = GetComponentsInChildren<Alvo>();
-                foreach (Alvo direction in script)
-                {
-                    direction.direction = 1;
-                }
-                break;
+    }
 
-            case 1100:
-                Instantiate(AlvoNormal, transform);
-                script = GetComponentsInChildren<Alvo>();
-                foreach (Alvo direction in script)
-                {
-                    direction.direction = 1;
-                }
-                break;
-
-            case 1400:
-                Instantiate(AlvoRapido, transform);
-                script = GetComponentsInChildren<Alvo>();
-                foreach (Alvo direction in script)
-                {
-                    direction.direction = 3;
-                }
-                break;
-
-            case 1425:
-                Instantiate(AlvoRapido, transform);
-                script = GetComponentsInChildren<Alvo>();
-                foreach (Alvo direction in script)
-                {
-                    direction.direction = 3;
-                }
-                break;
-
+    public IEnumerator Spawning()
+    {
+        
+        spawner = Random.Range(1, 4);
             
+            if(spawner == 3){
+                Instantiate(AlvoPulante, spawnCentro.transform);
+                script = spawnCentro.GetComponentsInChildren<Alvo>();
+                foreach (Alvo direction in script)
+                {
+                    direction.direction = 5;
+                }
+                yield return new WaitForSeconds(5f);
+            }
+            else
+            {
+                alvo = Random.Range(1, 4);
+                switch(spawner)
+                {
+                    case 1:
+                        if(alvo == 1){
+                            Instantiate(AlvoNormal, spawnEsquerda.transform);
+                            script = spawnEsquerda.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 1;
+                            }
+                            yield return new WaitForSeconds(5f);
+                        }
+                        if(alvo == 2){
+                            Instantiate(AlvoRapido, spawnEsquerda.transform);
+                            script = spawnEsquerda.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 3;
+                            }
+                            yield return new WaitForSeconds(3f);
+                        }
+                        if(alvo == 3){
+                            Instantiate(AlvoRapido, spawnEsquerda.transform);
+                            script = spawnEsquerda.GetComponentsInChildren<Alvo>();
+                            script = spawnEsquerda.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 3;
+                            }
 
-        }
+                            yield return new WaitForSeconds(0.25f);
 
+                            Instantiate(AlvoRapido, spawnEsquerda.transform);
+                            script = spawnEsquerda.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 3;
+                            }
+                            yield return new WaitForSeconds(3f);
+                        }
+                    break;
+                    case 2:
+                    if(alvo == 1){
+                            Instantiate(AlvoNormal, spawnDireita.transform);
+                            script = spawnDireita.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 2;
+                            }
+                            yield return new WaitForSeconds(5f);
+                        }
+                        if(alvo == 2){
+                            Instantiate(AlvoRapido, spawnDireita.transform);
+                            script = spawnDireita.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 4;
+                            }
+                            yield return new WaitForSeconds(3f);
+                        }
+                        if(alvo == 3){
+                            Instantiate(AlvoRapido, spawnDireita.transform);
+                            script = spawnDireita.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 4;
+                            }
+                            
+                            yield return new WaitForSeconds(0.25f);
+
+                            Instantiate(AlvoRapido, spawnDireita.transform);
+                            script = spawnDireita.GetComponentsInChildren<Alvo>();
+                            foreach (Alvo direction in script)
+                            {
+                                direction.direction = 4;
+                            }
+                            yield return new WaitForSeconds(3f);
+                        }
+                    break;
+                }
+            }
+        StartCoroutine(Spawning());
     }
 }
